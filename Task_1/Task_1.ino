2c26ef988146b68f6677b2d851b2ff137b175c78
Oliver <121129417+Zethos-02@users.noreply.github.com>
2023-06-13 11:00:12 +0100

    Updating task 1
    adding the library and comments onto the program

--- Task_1/Task_1.ino
+++ Task_1/Task_1.ino
@@ -1,24 +1,24 @@
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-int buttonApin = 2;
+#include <LiquidCrystal.h> //includes the liquid crystal library to connect to the lcd screen.
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //Initialises the pins for the lcd screen
+int buttonApin = 2; //Initialises the pin for the switch
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(buttonApin, INPUT_PULLUP);  
-  lcd.begin(16, 2);
-  lcd.print("Drive Disabled");
+  // Setup code to enable the correct pins and print out "Drive Disabled"
+  pinMode(buttonApin, INPUT_PULLUP); //Sets pin 2 to a switch input pin
+  lcd.begin(16, 2); //Turns the lcd screen on
+  lcd.print("Drive Disabled"); //Print "Drive Disabled" onto the lcd screen
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(buttonApin) == LOW)
+  // Main code that detects the state of the switch, and changes the displayed message accordingly
+  if (digitalRead(buttonApin) == LOW) //If the switch is low
   {
-    lcd.setCursor(0, 0);
-    lcd.print("Drive Enabled");
+    lcd.setCursor(0, 0); //Set the cursor to position 0,0 on the lcd screen
+    lcd.print("Drive Enabled"); //Print "Drive Enabled" onto the lcd screen
   }
-  if (digitalRead(buttonApin) == HIGH)
+  if (digitalRead(buttonApin) == HIGH) //If the switch is high
   {
-    lcd.setCursor(0, 0);
-    lcd.print("Drive Disabled");
+    lcd.setCursor(0, 0); //Set the cursor to position 0,0 on the lcd screen
+    lcd.print("Drive Disabled"); //Print "Drive Disabled" onto the lcd screen
   }
 }
 
